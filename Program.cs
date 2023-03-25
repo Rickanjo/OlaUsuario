fe83e724b631bd64f8dfd492c8700dc30d974bef
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-03-25 15:09:11 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Óla, usuario");
+
+Console.Write("Digite seu nome:");
+string NomeUsuario;
+
+NomeUsuario = Console.ReadLine()!;
+Console.WriteLine();
+
+Console.WriteLine($"Óla, {NomeUsuario}!");   
+
